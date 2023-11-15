b82419d92bf322890b39f63442747d271a3c36bb
shree-77 <shreedhoni7@gmail.com>
2023-11-15 21:47:32 +0530

    Education info added

--- src/Components/Education/EducationInfoSection.jsx
+++ src/Components/Education/EducationInfoSection.jsx
@@ -5,6 +5,16 @@ function EducationInfoSection(){
             <div className="Info-EduHeader">
                 <h3 className="section-title">Education Details</h3>
             </div>
+            <div className="Display-Education">
+                <div className="Date-Location">
+                   <p className="date-period"><span>Start Date</span>- <span>End Date</span></p>
+                   <p className="location"><span>Location</span></p>
+                </div>
+                <div className="Uni-dept">
+                    <p className="University"><span>University Name</span></p>
+                    <p className="Department"><span>Department Name</span></p>
+                </div>
+            </div>
         </div>
         </>
     )

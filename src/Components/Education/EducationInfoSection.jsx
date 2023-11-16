5bcfc9ba0420733c46e1c19b7328bb74e25165aa
shree-77 <shreedhoni7@gmail.com>
2023-11-16 11:24:34 +0530

    Sample data from example

--- src/Components/Education/EducationInfoSection.jsx
+++ src/Components/Education/EducationInfoSection.jsx
@@ -1,23 +1,30 @@
-function EducationInfoSection(){
-    return(
+
+import exampledata from "../../Exampledata";
+
+function EducationInfoSection() {
+    return (
         <>
-        <div className="container-EducationInfo">
-            <div className="Info-EduHeader">
-                <h3 className="section-title">Education Details</h3>
-            </div>
-            <div className="Display-Education">
-                <div className="Date-Location">
-                   <p className="date-period"><span>Start Date</span>- <span>End Date</span></p>
-                   <p className="location"><span>Location</span></p>
+            <div className="container-EducationInfo">
+                <div className="Info-EduHeader">
+                    <h3 className="section-title">Education Details</h3>
                 </div>
-                <div className="Uni-dept">
-                    <p className="University"><span>University Name</span></p>
-                    <p className="Department"><span>Department Name</span></p>
+                <div className="Display-Education">
+                    {exampledata.sections.educations.map((education) => (
+                        <div key={education.id} className="Education-Entry">
+                            <div className="Date-Location">
+                                <p className="date-period">{education.startDate} -  {education.endDate}</p>
+                                <p className="location"><span>Location:</span> {education.location}</p>
+                            </div>
+                            <div className="Uni-dept">
+                                <p className="University"><span>University Name:</span> {education.schoolName}</p>
+                                <p className="Department"><span>Degree:</span> {education.degree}</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
-        </div>
         </>
-    )
+    );
 }
 
 export default EducationInfoSection;
--- src/Components/Experience/ExperienceInfoSection.jsx
+++ src/Components/Experience/ExperienceInfoSection.jsx
@@ -1,3 +1,5 @@
+import exampledata from "../../Exampledata";
+
 function ExperienceInfoSection(){
     return(
         <>
@@ -6,19 +8,19 @@ function ExperienceInfoSection(){
                 <h3 className="section-title">Experience Details</h3>
             </div>
             <div className="Display-Experience">
-                <div className="Date-Location-exp">
-                   <p className="date-period"><span>Start Date</span>- <span>End Date</span></p>
-                   <p className="location"><span>Location</span></p>
-                </div>
-                <div className="company-designation">
-                    <p className="company"><span>Company Name</span></p>
-                    <p className="position"><span>Position</span></p>
-                    <p className="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    Consequuntur explicabo necessitatibus nostrum reiciendis 
-                    perspiciatis possimus voluptatem cupiditate sequi laboriosam 
-                    sed totam aperiam magni adipisci nisi exercitationem, ratione 
-                    tempore iste eos?</p>
-                </div>
+                {exampledata.sections.experiences.map((experience)=>
+                    <div key={experience.id} className="Experience-Entry">
+                        <div className="Data-Location-exp">
+                            <p className="date-period"><span>{experience.startDate}</span>-<span>{experience.endDate}</span></p>
+                            <p className="location"><span>{experience.location}</span></p>
+                        </div>
+                        <div className="company-designation">
+                            <p className="company"><span>{experience.companyName}</span></p>
+                            <p className="position"><span>{experience.positionTitle}</span></p>
+                            <p className="description">{experience.description}</p>
+                            </div>
+                    </div>
+                )}
             </div>
         </div>
         </>
